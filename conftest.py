3dee7cce10955fa29bc58fc2284b1afe01c1c43e
Chris Delaney <cjdelaney02@gmail.com>
2023-12-15 14:44:50 -0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -15,16 +15,18 @@ GOOGLE_SHEETS_TEST_DOCUMENT_ID = os.getenv("GOOGLE_SHEETS_TEST_DOCUMENT_ID")
 TEST_SLEEP = int(os.getenv("TEST_SLEEP", default="10"))
 
 def get_google_credentials():
-    # Check for JSON credentials in an environment variable (used in GitHub Actions)
-    credentials_env_var = os.getenv('GOOGLE_API_CREDENTIALS')
-    if credentials_env_var:
-        # Decode and load the JSON credentials from the environment variable
-        return json.loads(credentials_env_var)
+    # Check for the file path of the credentials in an environment variable
+    credentials_file_path = os.getenv('GOOGLE_CREDENTIALS_FILEPATH')
+    if credentials_file_path:
+        # Use the credentials file created by the GitHub Action
+        with open(credentials_file_path) as f:
+            return json.load(f)
     else:
         # Locally, use the 'google-credentials-test.json' file
         credentials_filepath = os.path.join(os.path.dirname(__file__), "google-credentials-test.json")
         with open(credentials_filepath) as f:
             return json.load(f)
+
 
 @pytest.fixture()  # scope="module"
 def ss():
